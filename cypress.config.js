0b5569f4982685debf46d044580054b531bbc79d
kkomaenga <a.aleksandrova1998@gmail.com>
2024-04-02 22:05:30 +0300

    Add mochawesome HTML report

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    },
+    specPattern: 'cypress/e2e/test.cy.js', 
+  },
+  component: {
+    setupNodeEvents(on, config) {
     },
   },
+  reporter: "mochawesome",
+  reporterOptions: {
+   
+    reportFilename: "mochawesome-report",
+  },
 });
+
